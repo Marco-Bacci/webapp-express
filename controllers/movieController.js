99d9b3a8f573d3b1ab406cd4e2f64a606f247ce3
Marco Bacci <marcobacci94@gmail.com>
2025-09-18 16:28:36 +0200

    feat: add index query

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,9 +1,17 @@
 // importo la connessione al db
-const connection = require("../data/db")
+const connection = require("../data/db");
 
 // index
 const index = (req, res) => {
-  res.send("lista movies");
+  const sql = "SELECT * FROM movies";
+  connection.query(sql, (err, results) => {
+    if (err) {
+      return res
+        .status(500)
+        .jason({ error: "errore nell'esecuzione della query:" + err });
+    }
+    res.send(results);
+  });
 };
 
 // show
